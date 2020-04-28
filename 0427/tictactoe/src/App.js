05e471d737f40a3cf61a7bcd339a7c8094707193
whatsquackin@hotmail.com <whatsquackin@hotmail.com>
2020-04-27 22:00:10 -0400

    part 2

--- 0427/tictactoe/src/App.js
+++ 0427/tictactoe/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 
-function Square(props) {
+ Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
